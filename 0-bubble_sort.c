fa2160b8f029af193fe1e3cf7c12095c351c1c21
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-20 14:38:17 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,21 +7,26 @@
 *@size: the size of the array
 */
 
-void bubble_sort(int *array, size_t size){
-    int i;
-    size_t x=0, y;
+void bubble_sort(int *array, size_t size)
+{
+int i;
+size_t x = 0, y;
 
-    if (!size || !array) 
-        return;
+if (!size || !array)
+return;
 
-    while (x < size){
-        for (y = 0; y < size - 1; y++){
-            if (array[y] > array[y + 1]){
-                i = array[y];
-                array[y] = array[y + 1];
-                array[y + 1] = i;
-                print_array(array, size);
-            }
-        }x++;
-    }
+while (x < size)
+{
+for (y = 0; y < size - 1; y++)
+{
+if (array[y] > array[y + 1])
+{
+i = array[y];
+array[y] = array[y + 1];
+array[y + 1] = i;
+print_array(array, size);
+}
+}
+x++;
+}
 }
