2cde4739f0ed6cc719e5e87e648f6d62d0db5cfa
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-11-20 16:11:30 -0800

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,23 +9,23 @@
 
 void selection_sort(int *array, size_t size)
 {
-size_t x, y, z;
-int temp;
+	size_t x, y, z;
+	int temp;
 
-for (x = 0; x < size - 1; x++)
-{
-z = x;
-for (y = x + 1; y < size; y++)
-{
-if (array[y] < array[z])
-z = y;
-}
-if (z != x)
-{
-temp = array[x];
-array[x] = array[z];
-array[z] = temp;
-print_array(array, size);
-}
-}
+	for (x = 0; x < size - 1; x++)
+	{
+		z = x;
+		for (y = x + 1; y < size; y++)
+		{
+			if (array[y] < array[z])
+			z = y;
+		}
+		if (z != x)
+		{
+			temp = array[x];
+			array[x] = array[z];
+			array[z] = temp;
+			print_array(array, size);
+		}
+	}
 }
